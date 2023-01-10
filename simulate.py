fc9e8d5aa678ed81770d639e1eaa4f7947edc7a1
buckiasp000 <58791683+buckiasp000@users.noreply.github.com>
2023-01-09 22:21:22 -0600

    More stuff

--- simulate.py
+++ simulate.py
@@ -1,1 +1,1 @@
-import pybullet as pimport time as tphysicsClient = p.connect(p.GUI)for i in range(1000):    p.stepSimulation()    t.sleep(1/60)    print(i)p.disconnect()
+import pybullet as pimport time as tphysicsClient = p.connect(p.GUI)p.loadSDF("box.sdf")for i in range(1000):    p.stepSimulation()    t.sleep(1/60)    print(i)p.disconnect()
